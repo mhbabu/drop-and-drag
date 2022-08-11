3a0e4b77f76d9c9129bfdc0e7d1020b9b0977876
mhbabu <write2babu.cse@gmail.com>
2022-08-11 10:04:22 +0600

    Drop and drag set up done

--- frontend/src/components/BoardArea.js
+++ frontend/src/components/BoardArea.js
@@ -5,6 +5,22 @@ import Form from "./Form";
 export default function BoardArea() {
   const [taskList, setTaskList] = useState();
 
+  const dragStarted = (e, id) => {
+    console.log("Draging over now");
+    e.dataTransfer.setData("todo", 'BABU');
+  };
+
+  const draggingOver = (e) => {
+    e.preventDefault();
+    console.log("Drag Started");
+  };
+
+  const dragDropped = (e) => {
+    console.log("You have dropped");
+    let transferData = e.dataTransfer.getData("todo");
+    console.log(transferData);
+  };
+
   const fetchData = async () => {
     let { data: tasks } = await getTasks();
     setTaskList(tasks);
@@ -15,40 +31,61 @@ export default function BoardArea() {
   }, []);
 
   return (
-    <div className='card'>
-      <div className='card-body mt-5'>
+    <div className="card">
+      <div className="card-body mt-5">
         <Form taskList={taskList} setTaskList={setTaskList} />
-        <div className='row'>
-          <div className='col-md-12 mt-5'>
-            <div className='board-area'>
-              <div className='column'>
+        <div className="row">
+          <div className="col-md-12 mt-5">
+            <div className="board-area">
+              <div className="column">
                 <h1>To Do</h1>
                 {taskList?.map(
                   (task) =>
                     task.status === "To Do" && (
-                      <div draggable key={task?._id} className='item'>
+                      <div
+                        key={task?._id}
+                        draggable
+                        onDragStart={(e) => dragStarted(e, task._id)}
+                        onDragOver={(e) => draggingOver(e)}
+                        onDrop={(e) => dragDropped(e)}
+                        className="item"
+                      >
                         {task.name}
                       </div>
                     )
                 )}
               </div>
-              <div className='column'>
+              <div className="column">
                 <h1>In progress</h1>
                 {taskList?.map(
                   (task) =>
                     task.status === "Progress" && (
-                      <div draggable key={task?._id} className='item'>
+                      <div
+                        key={task?._id}
+                        draggable
+                        onDragStart={(e) => dragStarted(e, task._id)}
+                        onDragOver={(e) => draggingOver(e)}
+                        onDrop={(e) => dragDropped(e)}
+                        className="item"
+                      >
                         {task.name}
                       </div>
                     )
                 )}
               </div>
-              <div className='column'>
+              <div className="column">
                 <h1>Completed</h1>
                 {taskList?.map(
                   (task) =>
                     task.status === "Complete" && (
-                      <div draggable key={task?._id} className='item'>
+                      <div
+                        key={task?._id}
+                        draggable
+                        onDragStart={(e) => dragStarted(e, task._id)}
+                        onDragOver={(e) => draggingOver(e)}
+                        onDrop={(e) => dragDropped(e)}
+                        className="item"
+                      >
                         {task.name}
                       </div>
                     )
